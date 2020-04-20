50e5a1b05af01b9de72dbc05254de977fc0de3ec
Pramod Rawtae <rawatepramosmd@gmail.com>
2020-04-20 15:06:44 +0530

    Section 4 :- Mulitple XML Config Files

--- spring-core-framework-the-easy-way/src/main/java/springDI/App.java
+++ spring-core-framework-the-easy-way/src/main/java/springDI/App.java
@@ -1,26 +1,25 @@
 package springDI;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import config.JavaConfig;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		/* Application context parses the sml file, identifies the classes/components/beans.
+		/* Application context parses the xml file, identifies the classes/components/beans.
 		 * When program is executed it create all the instantiates of the classes.
 		 * it also identifies the dependencies required by the classes and injects the dependencies.
 		 */
 		try {
 			//Bean using xml Config
-			//ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
-			//AnimalBean bean = (AnimalBean) context.getBean("animalBean");
+			ApplicationContext context = new ClassPathXmlApplicationContext("Config2.xml");
+			AnimalBean bean = (AnimalBean) context.getBean("animalBean");
 			
 			//Bean using Java COnfig
-			ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
-			AnimalBean bean = (AnimalBean) context.getBean("animalBean");
+			//ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+			//AnimalBean bean = (AnimalBean) context.getBean("animalBean");
+			
 			bean.shout();
 		}
 		catch(Exception e) {
