1a43eb8ea45b295a1e8fcf06d6e45bf4745b2471
Pramod Rawtae <rawatepramosmd@gmail.com>
2020-04-20 15:27:29 +0530

    Section 4 :- Both XML and Java Config files together

--- spring-core-framework-the-easy-way/src/main/java/config/JavaConfig.java
+++ spring-core-framework-the-easy-way/src/main/java/config/JavaConfig.java
@@ -2,13 +2,13 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 import springDI.Animal;
 import springDI.AnimalBean;
 
 @Configuration
-@Import(JavaConfig2.class)
+@ImportResource("classpath:Config2.xml")
 public class JavaConfig {
 	
 	@Bean
