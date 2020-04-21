a754a002d282a3bed1c968186d4d8151a5157485
Pramod Rawtae <rawatepramosmd@gmail.com>
2020-04-21 19:49:06 +0530

    Section 5.4 :- Setter Injection using Java Configuration

--- spring-app/src/main/java/com/pramod/app/MyApp.java
+++ spring-app/src/main/java/com/pramod/app/MyApp.java
@@ -1,17 +1,20 @@
 package com.pramod.app;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.pramod.config.Config;
 
 public class MyApp {
 	public static void main(String args[]) {
 
 		//XML Config
-		ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
-		ManageAppData appData = (ManageAppData) context.getBean("manageAppData");		
+		//ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
+		//ManageAppData appData = (ManageAppData) context.getBean("manageAppData");		
 		
 		//Java Config
-		//ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);		
-		//ManageAppData appData = (ManageAppData) context.getBean("manageAppData");
+		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);		
+		ManageAppData appData = (ManageAppData) context.getBean("manageAppData");
+		
 		appData.accessData();
 		appData.storeData();
 		appData.disconnect();
